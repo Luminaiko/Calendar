b71414f44d0d84baecaa7f786a1dd014486e229a
Gaskov Vadim <vadik.gaskoff@yandex.ru>
2023-06-24 14:23:14 +0800

    AddedBottomPanelToShowRoomsAndTime

--- Calendar/wwwroot/js/calendarScript.js
+++ Calendar/wwwroot/js/calendarScript.js
@@ -1,55 +1,96 @@
-﻿const daysTag = document.querySelector(".days"),
-    currentDate = document.querySelector(".current-date"),
-    prevNextIcon = document.querySelectorAll(".icons span");
+﻿const daysTag = document.querySelector(".days");
+const currentDate = document.querySelector(".current-date");
+const prevNextIcon = document.querySelectorAll(".icons span");
+const selectedDateElement = document.getElementById("selected-date");
+const bookButton = document.getElementById("book-button");
 
-// getting new date, current year and month
-let date = new Date(),
-    currYear = date.getFullYear(),
-    currMonth = date.getMonth();
+// Получение новой даты, текущего года и месяца
+let date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
 
-// storing full name of all months in array
-const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль",
-    "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+// Сохранение полного названия всех месяцев в массиве
+const months = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь"
+];
 
 const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
+    let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
     let liTag = "";
 
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+    for (let i = firstDayofMonth; i > 0; i--) {
         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
 
-    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
-        // adding active class to li if the current day, month, and year matched
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-            && currYear === new Date().getFullYear() ? "active" : "";
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday =
+            i === date.getDate() &&
+                currMonth === new Date().getMonth() &&
+                currYear === new Date().getFullYear()
+                ? "active"
+                : "";
         liTag += `<li class="${isToday}">${i}</li>`;
     }
 
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    for (let i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
     }
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
-}
+
+    // Добавляем обработчик событий для выбора даты
+    const days = document.querySelectorAll(".days li");
+    days.forEach(day => {
+        day.addEventListener("click", () => {
+            if (!day.classList.contains("inactive")) {
+                const selectedDate = day.textContent;
+                const selectedMonth = months[currMonth];
+                selectedDateElement.textContent = `${selectedDate} ${selectedMonth} ${currYear}`;
+            }
+        });
+    });
+};
+
 renderCalendar();
 
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+bookButton.addEventListener("click", () => {
+    const selectedDate = selectedDateElement.textContent;
+    const selectedTime = document.getElementById("booking-time-start").value;
+    console.log(selectedTime);
+    const selectedTimeEnd = document.getElementById("booking-time-end").value;
+    console.log(selectedTimeEnd);
+    const selectedRoom = document.getElementById("booking-room").value;
+
+    // Выполните нужные действия при нажатии на кнопку "Забронировать"
+    // Например, отправка данных на сервер или другие операции
+});
+
+prevNextIcon.forEach((icon) => {
+    icon.addEventListener("click", () => {
         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 
-        if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
+        if (currMonth < 0 || currMonth > 11) {
             date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
         } else {
-            date = new Date(); // pass the current date as date value
+            date = new Date();
         }
-        renderCalendar(); // calling renderCalendar function
+        renderCalendar();
     });
 });
